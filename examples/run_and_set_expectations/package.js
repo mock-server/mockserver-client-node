3578734fef25d74ddb241bb5fcec1d3f345c74d6
James Bloom <jamesdbloom@gmail.com>
2017-11-18 06:16:08 +0000

    added example code

---
+++ examples/run_and_set_expectations/package.js
@@ -0,0 +1,15 @@
+{
+  "name": "launch_and_set_expectations",
+  "version": "1.0.0",
+  "description": "",
+  "main": "launch_and_set_expectations.js",
+  "scripts": {
+    "start": "node launch_and_set_expectations.js"
+  },
+  "author": "James D Bloom",
+  "license": "Apache-2.0",
+  "dependencies": {
+    "mockserver-client": "^1.0.16",
+    "mockserver-node": "^1.1.1"
+  }
+}
