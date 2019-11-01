2446709d31928b99715bd0ef17f6554c79474414
James Bloom <733179+jamesdbloom@users.noreply.github.com>
2019-11-01 13:48:16 +0000

    fixed docker tag

--- scripts/local_docker_launch.sh
+++ scripts/local_docker_launch.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker pull jamesdbloom/mockserver:maven
+docker pull jamesdbloom/mockserver:grunt
 docker run -i -t --memory=4096m --oom-kill-disable -v `pwd`:/mockserver -w /mockserver -a stdout -a stderr jamesdbloom/mockserver:grunt /bin/bash
