e0aa5bc98761aaf7196f9884c2930163c4f43af3
James D Bloom <733179+jamesdbloom@users.noreply.github.com>
2022-01-19 09:43:52 +0000

    updated all possible dependencies

--- scripts/local_docker_build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-docker pull mockserver/mockserver:grunt
-docker run -v `pwd`:/mockserver -w /mockserver -a stdout -a stderr mockserver/mockserver:grunt /mockserver/scripts/local_quick_build.sh
